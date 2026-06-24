992d95e38da0775aadb2bff6ccbf029ad0d43acc
Ishak Sebsib <ishaksebsib@gmail.com>
2026-06-24 19:32:09 +0300

    chore: remove leaded map files

--- packages/orchestrator/tests/support/store-contract.d.ts
+++
@@ -1,7 +0,0 @@
-import { type Store } from "../../src";
-export interface StoreContractSuiteInput {
-    readonly name: string;
-    readonly withStore: <A>(use: (store: Store) => Promise<A>) => Promise<A>;
-}
-export declare function registerStoreContractTests(input: StoreContractSuiteInput): void;
-//# sourceMappingURL=store-contract.d.ts.map
